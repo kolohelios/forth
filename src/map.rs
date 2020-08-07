0c6cda44da3a9e8401a98219c4558fd9da66b5eb
Jon Edwards <jkedwards@me.com>
2020-08-07 05:39:14 +0000

    add basic map structure

--- src/map.rs
+++ src/map.rs
@@ -1,9 +1,49 @@
-pub mod map {}
+pub mod map {
+    pub enum MapElement {
+        Water { height: i8 },
+        Land { height: i8 },
+        Void,
+    }
+
+    pub struct Map {
+        pub content: Vec<Vec<MapElement>>,
+    }
+
+    impl Map {
+        pub fn new(width: i8, height: i8) -> Map {
+            let mut content = vec![vec![]];
+
+            for x in 0..width - 1 {
+                for y in 0..height - 1 {
+                    content[x as usize][y as usize] = MapElement::Water { height: 0 }
+                }
+            }
+
+            Map { content }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn map_creation() {
-        assert_eq!(true, true);
+    fn test_empty_map_creation() {
+        assert_eq!(
+            map::Map::new(0, 0),
+            map::Map {
+                content: vec![vec![]]
+            }
+        );
+    }
+
+    fn test_small_water_world() {
+        assert_eq!(
+            map::Map::new(1, 1),
+            map::Map {
+                content: vec![vec![map::MapElement::Water { height: 0 }]]
+            }
+        )
     }
 }
