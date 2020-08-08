0577977f31e3c87bdcdeadf8fbc42ecc3fe86669
Jon Edwards <jkedwards@me.com>
2020-08-08 23:09:55 +0000

    naive experimentation with mpsc threading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::{Duration, Instant};
+
 mod map;
 
 fn main() {
-    println!("Hello, world!");
+    let (clock_tx, clock_rx) = mpsc::channel();
+
+    let handle = thread::spawn(move || clock(clock_tx));
+    thread::spawn(move || display_clock(clock_rx));
+
+    handle.join().unwrap();
+}
+
+fn clock(tx: mpsc::Sender<String>) {
+    let now = Instant::now();
+    for i in 1..10 {
+        let message = format!("time is {}", now.elapsed().as_millis());
+        tx.send(message).unwrap();
+        thread::sleep(Duration::from_secs(5));
+    }
+}
+
+fn display_clock(rx: mpsc::Receiver<String>) {
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
