fe2b61faa063c965eb4a1453dbec6c326f23fc7b
Jon Edwards <jkedwards@me.com>
2020-08-16 20:39:18 +0000

    make the draw_pot function more useful and create a more general create empty space function

--- src/flower.rs
+++ src/flower.rs
@@ -1,4 +1,4 @@
-use cursive::theme::Color;
+use cursive::theme::{Color, ColorStyle};
 use machine::*;
 
 machine!(
@@ -131,6 +131,7 @@ impl FlowerColor {
     pub const BLACK: Color = Color::RgbLowRes(0, 0, 0);
     pub const BLUE: Color = Color::RgbLowRes(1, 2, 5);
     pub const WHITE: Color = Color::RgbLowRes(5, 5, 5);
+    pub const LIGHT_BROWN: Color = Color::RgbLowRes(3, 1, 0);
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -140,20 +141,46 @@ pub struct FlowerGlyphxel {
     character: char,
 }
 
-pub fn draw_flower() -> Vec<Vec<FlowerGlyphxel>> {
-    const WIDTH: usize = 7;
-    const HEIGHT: usize = 7;
+type DrawnFlower = Vec<Vec<FlowerGlyphxel>>;
 
-    let mut flower = vec![
+fn draw_pot(mut drawn_flower: DrawnFlower) -> DrawnFlower {
+    let x_max = drawn_flower[0].len();
+    let y_max = drawn_flower[0].len();
+
+    for x in 0..x_max {
+        let dirt_color = FlowerColor::LIGHT_BROWN;
+        let background_color = FlowerColor::WHITE;
+
+        let character;
+        if x == 0 {
+            character = '\\';
+        } else if x == x_max - 1 {
+            character = '/';
+        } else {
+            character = '░';
+        }
+
+        drawn_flower[0][x] = FlowerGlyphxel {
+            foreground_color: dirt_color,
+            background_color: background_color,
+            character,
+        };
+    }
+
+    drawn_flower
+}
+
+pub fn draw_empty_space(width: usize, height: usize) -> DrawnFlower {
+    let flower = vec![
         vec![
             FlowerGlyphxel {
                 foreground_color: FlowerColor::WHITE,
                 background_color: FlowerColor::BLACK,
                 character: ' '
             };
-            HEIGHT
+            width
         ];
-        WIDTH
+        height
     ];
 
     flower
@@ -188,8 +215,55 @@ mod tests {
     }
 
     #[test]
+    fn test_draw_flower_pot() {
+        let flower = draw_empty_space(7, 1);
+        let flower_pot = draw_pot(flower);
+
+        assert_eq!(
+            flower_pot,
+            vec![vec![
+                FlowerGlyphxel {
+                    foreground_color: FlowerColor::LIGHT_BROWN,
+                    background_color: FlowerColor::WHITE,
+                    character: '\\'
+                },
+                FlowerGlyphxel {
+                    foreground_color: FlowerColor::LIGHT_BROWN,
+                    background_color: FlowerColor::WHITE,
+                    character: '░'
+                },
+                FlowerGlyphxel {
+                    foreground_color: FlowerColor::LIGHT_BROWN,
+                    background_color: FlowerColor::WHITE,
+                    character: '░'
+                },
+                FlowerGlyphxel {
+                    foreground_color: FlowerColor::LIGHT_BROWN,
+                    background_color: FlowerColor::WHITE,
+                    character: '░'
+                },
+                FlowerGlyphxel {
+                    foreground_color: FlowerColor::LIGHT_BROWN,
+                    background_color: FlowerColor::WHITE,
+                    character: '░'
+                },
+                FlowerGlyphxel {
+                    foreground_color: FlowerColor::LIGHT_BROWN,
+                    background_color: FlowerColor::WHITE,
+                    character: '░'
+                },
+                FlowerGlyphxel {
+                    foreground_color: FlowerColor::LIGHT_BROWN,
+                    background_color: FlowerColor::WHITE,
+                    character: '/'
+                },
+            ]]
+        )
+    }
+
+    #[test]
     fn test_draw_flower() {
-        let flower = draw_flower();
+        let flower = draw_empty_space(7, 7);
         assert_eq!(
             flower,
             vec![
